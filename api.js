159f852b350d9de2eb5765fcfebddc8e5b3735a5
Guilherme Mazur <jjguilh@gmail.com>
2022-07-12 02:04:06 -0300

    feat: added html, css, js and assets

---
+++ api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'api_key=6a5a10cc8eb3b0b6fcf35ee7fc658644';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
---
+++ main.js
@@ -0,0 +1,42 @@
+import {
+    API_KEY, BASE_URL,
+    IMG_URL,
+    language,
+  } from './api.js'
+
+const button = document.getElementById('button');
+
+$("button").click(function(){
+    let randomNumber = String(Math.floor(Math.random()*898143)+55);
+    let movieURL = BASE_URL+randomNumber+'?'+API_KEY+'&'+language;
+    console.log(movieURL);
+    $.getJSON(movieURL, function(result){
+        document.querySelector('#movie').style.minHeight = '300px';
+        if (result.poster_path != null){
+            document.querySelector('#poster').removeAttribute('style');
+            document.querySelector('#poster').setAttribute('src', IMG_URL + result.poster_path);
+        }
+        else{
+            document.querySelector('#poster').setAttribute('src', './assets/poster-not-available.jpg');
+        };
+        document.querySelector('#movietitle').textContent = result.original_title;
+        document.querySelector('#overview').textContent = result.overview;    
+    })
+    .fail(function() {             
+        document.querySelector('#poster').removeAttribute('style');
+        document.querySelector('#poster').setAttribute('src', './assets/poster-not-available.jpg');
+        document.querySelector('#movietitle').textContent = 'Movie not available';
+        document.querySelector('#overview').textContent = 'Movie not available';    
+    });
+  });
+
+
+//function getMovie(){
+//    let randomNumber = String(Math.floor(Math.random()*898143)+55);
+//    let movieURL = BASE_URL+randomNumber+'?'+API_KEY;
+//    $.getJSON(movieURL, function(data){
+//        movieData = data;  
+//    });
+//    console.log(movieData);    
+//};
+
